035008d7e88060a3b4aae4953391795205c15823
Kevin Estevez <kjestevez.0795@gmail.com>
2023-04-15 13:27:49 -0700

    Fix Column order

--- chessboard/src/stores/chessboard.ts
+++ chessboard/src/stores/chessboard.ts
@@ -5,7 +5,7 @@ const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 const rows = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 export const useChessBoardStore = defineStore('chessboard', () => {
-  const positions = computed(() => cols.map((col) => rows.map((row) => `${col}${row}`)))
+  const positions = computed(() => rows.map((row) => cols.map((col) => `${col}${row}`)))
   const clicked = ref([] as string[])
   const hasBeenClicked = (value: string) => clicked.value.includes(value)
   const onClick = (value: string) => clicked.value.push(value)
