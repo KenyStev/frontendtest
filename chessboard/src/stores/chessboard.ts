265d907564b6ebf95b4877bc2e1c1b5535b101ab
kenystev <kjestevez.0795@gmail.com>
2023-12-14 16:59:00 -0700

    Fix positions and double click

--- chessboard/src/stores/chessboard.ts
+++ chessboard/src/stores/chessboard.ts
@@ -1,14 +1,14 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].reverse()
 const rows = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 export const useChessBoardStore = defineStore('chessboard', () => {
   const positions = computed(() => rows.map((row) => cols.map((col) => `${col}${row}`)))
   const clicked = ref([] as string[])
   const hasBeenClicked = (value: string) => clicked.value.includes(value)
-  const onClick = (value: string) => clicked.value.push(value)
+  const onClick = (value: string) => !hasBeenClicked(value) && clicked.value.push(value)
 
   return { positions, hasBeenClicked, onClick, clicked }
 })
